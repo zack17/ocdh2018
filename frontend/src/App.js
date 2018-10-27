4fac7ead0805004fb17f587c5a7706fedeebda2c
Micha Reiser <micha@famreiser.ch>
2018-10-27 11:52:45 +0200

    List objects

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Query
+          query={gql`     
+          {
+            objects {
+              classification {
+                name
+              }
+            }
+          }
+          `}
+        >
+          {({ loading, error, data }) => {
+            if (loading) return <p>Loading...</p>;
+            if (error) return <p>Error :(</p>;
+
+            return data.objects.map(({ classification }) => (
+              <div key={classification.name}>
+                <p>{classification.name}</p>
+              </div>
+            ));
+          }}
+        </Query>
       </div>
     );
   }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,32 +4,21 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import ApolloClient from "apollo-boost";
-
-import gql from "graphql-tag";
+import { ApolloProvider } from "react-apollo";
 
 const client = new ApolloClient({
   uri: "https://api-euwest.graphcms.com/v1/cjnr7i2yb0bje01glv5s68dio/master",
   headers: {
-    Authorization:
-      `Bearer ${process.env.REACT_APP_GRAPH_CMS_TOKEN}`
+    Authorization: `Bearer ${process.env.REACT_APP_GRAPH_CMS_TOKEN}`
   }
 });
 
-client
-  .query({
-    query: gql`
-      {
-        objects {
-          classification {
-            name
-          }
-        }
-      }
-    `
-  })
-  .then(result => console.log(result));
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
